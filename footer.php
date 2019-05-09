e1a92898e3db2a58dbf2c349def0eae3d6131fce
hluce1 <ashleigh.breakey@gmail.com>
2019-05-09 12:19:32 +1000

    making a new footer

--- footer.php
+++ footer.php
@@ -1,34 +1,68 @@
 </div> <!-- end container -->
-<footer role="contentinfo" class="footer navbar navbar-fixed-bottom container-fluid">
-  <div class="container">
-    <div class="row">
-      <div id="inner-footer" class="inner-footer clearfix col-sm-8 col-sm-offset-2">
-        <div id="widget-footer" class="clearfix">
-          <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1')) : ?>
-          <?php endif; ?>
-          <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2')) : ?>
-          <?php endif; ?>
-          <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3')) : ?>
-          <?php endif; ?>
-        </div>
-        <nav class="clearfix">
-          <?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
-        </nav>
-        <p class="pull-right">
-        <p class="footer-middle"> This website was produced by students in the
+<footer>
+ 
+<!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h2>Quick links</h2>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h2>Quick links</h2>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h2>Quick links</h2>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+						<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+				</div>
+				</hr>
+			</div>	
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p class="footer-middle"> This website was produced by students in the
           Faculty of Arts & Design, University of Canberra. </p>
         <p class="footer-middle"> The content is extracted from the Tocumwal
-          Houses Archive in the <a
+          Houses Archive in the <a class="external-link1"
               href="https://www.library.act.gov.au/find/history/search/Manuscript_Collections/hmss_0074_tocumwal_houses_archive">
             ACT Heritage Library Manuscripts Collection </a></p>
         </p>
-<!--        <p class="pull-right"><a href="http://320press.com" id="credit320"-->
-<!--                                 title="By the dudes of 320press">320press</a></p>-->
-        <p class="attribution">&copy; <?php bloginfo('name'); ?></p>
-      </div>
-    </div>
-  </div>
-  <!-- end #inner-footer -->
+				</div>
+				</hr>
+			</div>	
+		</div>
+	</section>
+	<!-- ./Footer -->
 </footer>
 <!-- end footer -->
 <!--[if lt IE 7 ]>
